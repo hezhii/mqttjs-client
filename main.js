8154f2206c91d0702e4d502dde9dc33c5e0c7c72
biu <hezhou920@gmail.com>
2017-09-10 22:41:29 +0800

    subscribe

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-let connectionForm, client, state;
+let connectionForm, subscriptionForm, state, tbody;
+
+let client;
 
 $(function () {
   $('#color').colorpicker();
-  $('#connectButton').click(connect);
+  $('#connectButton').click(handleConnect);
+  $('#subscribeButton').click(handleSubscribe);
 });
 
-function connect(event) {
+function handleConnect(event) {
   connectionForm = connectionForm || $('#connectionForm');
-  let data = connectionForm.serializeArray();
-  let formData = {};
-
-  data.forEach(function (item) {
-    if (item.value) {
-      formData[item.name] = item.value;
-    }
-  });
+  const formData = convertFormData(connectionForm.serializeArray());
 
   client = mqtt.connect(`ws://${formData.host}:${formData.port}/mqtt`, {
     username: formData.username,
@@ -30,3 +26,48 @@ function connect(event) {
 
   event.preventDefault();
 }
+
+function handleSubscribe(event) {
+  subscriptionForm = subscriptionForm || $('#subscriptionForm');
+  const formData = convertFormData(subscriptionForm.serializeArray());
+
+  const {
+    subscribeTopic: topic,
+    subscribeQoS: qos,
+    color
+  } = formData;
+
+  if (client) {
+    client.subscribe(topic, {'qos': parseInt(qos)}, function (err) {
+      if (err) {
+        alert(`There has some problems when subscribe topic "${formData.topic}"!\nError:${err}`);
+      } else {
+        tbody = tbody || $('tbody');
+        tbody.append(`<tr><td>${topic}</td><td><i style="background: ${color}"></i></td><td>${qos}</td></tr>`);
+      }
+    });
+  } else {
+    alert('You have to create connection first!');
+  }
+
+  event.preventDefault();
+}
+
+/**
+ * 将 jQuery 序列化后的表单数据数组转换为对象
+ * @param {Array} arr
+ * @return {Object}
+ */
+function convertFormData(arr) {
+  let obj = {};
+  if (!arr || !arr.length) {
+    return obj;
+  }
+  arr.forEach(function (item) {
+    if (item.value) {
+      obj[item.name] = item.value;
+    }
+  });
+  return obj;
+}
+
