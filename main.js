1e86c7135ed6b224f2dfd2f19f0a56a68bd8008d
biu <hezhou920@gmail.com>
2017-09-10 21:29:56 +0800

    connected

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+let connectionForm, client, state;
+
 $(function () {
   $('#color').colorpicker();
+  $('#connectButton').click(connect);
 });
+
+function connect(event) {
+  connectionForm = connectionForm || $('#connectionForm');
+  let data = connectionForm.serializeArray();
+  let formData = {};
+
+  data.forEach(function (item) {
+    if (item.value) {
+      formData[item.name] = item.value;
+    }
+  });
+
+  client = mqtt.connect(`ws://${formData.host}:${formData.port}/mqtt`, {
+    username: formData.username,
+    password: formData.password,
+    clientId: formData.clientId
+  });
+
+  client.on('connect', function () {
+    state = state || $('#state');
+    state.attr('class', 'color-green');
+    state.children('span').text('connected');
+  });
+
+  event.preventDefault();
+}
